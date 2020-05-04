36580691d840a82b3022328e7695549cd5d73540
wt1814 <wangtao1814@163.com>
2020-05-05 00:46:56 +0800

    day

--- cn.com/test.java
+++ cn.com/test.java
@@ -10,16 +10,7 @@
  * @Version: V1.0
  **/
 public class test {
-    public void write(String key,Object data){
-        //1.删除缓存
-        redis.delKey(key);
-        //2.更新数据
-        db.updateData(data);
-        //3.休眠1S
-        Thread.sleep(1000);
-        //2.再次删除缓存
-        redis.delKey(key);
-    }
+
 }
 
 
