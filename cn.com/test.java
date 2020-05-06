31523d274f58d0c5854378169f48fb2ed79139d2
wt1814 <wangtao1814@163.com>
2020-05-06 18:00:44 +0800

    day

--- cn.com/test.java
+++ cn.com/test.java
@@ -11,6 +11,35 @@
  **/
 public class test {
 
+
+    /**
+     * The head (eldest) of the doubly linked list.
+     */
+    transient LinkedHashMap.Entry<K,V> head;
+
+    /**
+     * The tail (youngest) of the doubly linked list.
+     */
+    transient LinkedHashMap.Entry<K,V> tail;
+    //链接新加入的p节点到链表后端
+    private void linkNodeLast(LinkedHashMap.Entry<K,V> p) {
+        LinkedHashMap.Entry<K,V> last = tail;
+        tail = p;
+        if (last == null)
+            head = p;
+        else {
+            p.before = last;
+            last.after = p;
+        }
+    }
+    //LinkedHashMap的节点类
+    static class Entry<K,V> extends HashMap.Node<K,V> {
+        Entry<K,V> before, after;
+        Entry(int hash, K key, V value, Node<K,V> next) {
+            super(hash, key, value, next);
+        }
+    }
+
 }
 
 
